95c3a86712d09ac1920516bc6a941fabb569f0c1
SuperSpawn <thearktitan@gmail.com>
2023-02-04 15:43:05 +0200

    added basic world generation

--- js/game.js
+++ js/game.js
@@ -12,11 +12,21 @@ const tools = {
     'pickaxe': ["cobblestone", "stone"],
     'shovel': ["dirt", "grass"],
     'axe': ["log"],
-    'bucket': ["lava", "water"],
-    'shears': ["leaf"]
+    'bucket': ["water"],
+    'shears': ["leaf"],
+    'bucket-water': ["empty"],
+    'bucket-lava': ["empty"]
 };
+const afterTools = {
+    'bucket-water': 'water',
+}
+const toolAfterUse = {
+    'bucket-water': 'bucket',
+    'bucket': 'bucket-water'
+}
 
 
+//call function upon click
 
 
 const blockGrid = document.querySelector('#block-grid');
@@ -27,10 +37,8 @@ const hotBar = document.querySelector('#hot-bar');
 selectedObject = toolBar.querySelector('.pickaxe');;
 
 
-function loadEmptyTemplate(swidth, sheight) {
+function loadEmptyTemplate(width, height) {
     let i, j;
-    let width = Math.floor(swidth / 40);
-    let height = Math.floor(sheight / 40);
 
     for(i = 0; i < height; ++i) {
         let children = document.createElement('div');
@@ -58,6 +66,75 @@ function loadEmptyTemplate(swidth, sheight) {
     blockGrid.appendChild(finalRow);
 }
 
+function sineNoise(value, intensity) {
+    return Math.sin(value) * intensity;
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function setSlotAt(x, y, type) {
+    let children = blockGrid.children;
+    let grandchildren = children[y].children;
+    let slot = grandchildren[x];
+
+    let oldType = slot.getAttribute('block-type');
+    slot.setAttribute('block-type', type);
+    slot.classList.remove(oldType);
+    slot.classList.add(type);
+}
+function loadRandomWorld(width, height,
+     maxGround, minGround,
+      intensity,
+       dirtMinDepth, dirtMaxDepth) {
+
+    loadEmptyTemplate(width, height);
+
+    let treePos = 4 + getRandomInt(width - 5);
+
+
+    let noise = sineNoise();
+    let startBlock = minGround + getRandomInt(maxGround - minGround);
+    let i, j;
+
+    for(i = 0; i < width; ++i) {
+        setSlotAt(i, startBlock, 'grass');
+        let dirtDepth = dirtMinDepth + getRandomInt(dirtMaxDepth - dirtMinDepth);
+        for(j = 1; j < dirtDepth; ++j) {
+            setSlotAt(i, startBlock + j, 'dirt');
+        }
+        while(startBlock + j < height) {
+            setSlotAt(i, startBlock + j, 'stone');
+            ++j;
+        }
+
+
+        startBlock = sineNoise(startBlock, intensity);
+        if(startBlock > maxGround){
+            startBlock = maxGround - getRandomInt(4);
+        }
+        if(startBlock < minGround) {
+            startBlock = minGround + getRandomInt(4);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
 
 
 function getTypeFromInventory(type) {
@@ -81,6 +158,13 @@ function setTypeFromInventory(type, number) {
     }
 }
 
+function updateTool(tool) {
+    if(toolAfterUse[tool] == undefined) return;
+    let newState = toolAfterUse[tool];
+    selectedObject.classList.remove(tool);
+    selectedObject.setAttribute('tool-type', newState);
+    selectedObject.classList.add(newState);
+}
 
 function interactWithTool(slot) {
     if(slot.getAttribute('block-type') == 'bedrock')
@@ -98,9 +182,13 @@ function interactWithTool(slot) {
         setTypeFromInventory(blockType, i);
     }
 
+    let after = afterTools[toolType];
+    if(after == undefined) after = 'empty';
+
     slot.classList.remove(slot.getAttribute('block-type'));
-    slot.setAttribute('block-type', 'empty');
-    slot.classList.add("empty");
+    slot.setAttribute('block-type', afterTools[toolType]);
+    slot.classList.add(after);
+    updateTool(toolType);
 }
 function interactWithBlock(slot) {
     if(slot.getAttribute('block-type') == 'empty') {
@@ -121,9 +209,7 @@ function interactWithBlock(slot) {
     }
 }
 function unsafeInteractWithBlock(slot) {
-    slot.classList.remove('empty');
-    slot.classList.add(selectedObject.getAttribute('block-type'));
-    slot.setAttribute('block-type', selectedObject.getAttribute('block-type'));
+    let theClass = selectedObject.getAttribute('block-type');
 
     let bottom = getBlockBelow(slot);
     if(bottom.getAttribute('block-type') == 'grass') {
@@ -131,6 +217,17 @@ function unsafeInteractWithBlock(slot) {
         bottom.classList.add('dirt');
         bottom.setAttribute('block-type', 'dirt');
     }
+    let top = getBlockAbove(slot);
+    if(top.getAttribute('block-type') != 'empty') {
+        if(selectedObject.getAttribute('block-type') == 'grass') {
+            theClass = 'dirt';
+        }
+    }
+
+
+    slot.classList.remove('empty');
+    slot.classList.add(theClass);
+    slot.setAttribute('block-type', theClass);
 }
 function getBlockBelow(slot) {
     let column = slot.getAttribute('row');
@@ -138,6 +235,13 @@ function getBlockBelow(slot) {
     let below = cousins[column];
     return below;
 }
+function getBlockAbove(slot) {
+    let column = slot.getAttribute('row');
+    let cousins = slot.parentNode.previousSibling.children;
+    let below = cousins[column];
+    return below;
+}
+
 
 function interactWithSlot(slot) {
     
@@ -189,10 +293,18 @@ function addHotBarButtons() {
 }
 
 
-loadEmptyTemplate(screen.width, screen.height);
+loadRandomWorld(100,100,
+    8, 12,
+    0.01,
+    2, 5);
 addBlockButtons();
 addHotBarButtons();
 addToolBarButtons();
 
 // function updateScreen()  
+
+
+
+
+
 
